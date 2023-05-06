fd9ace5983ecc435b8da8bc2f73ce641ec855604
sarfarazalam13 <alamsarfaraz386@gmail.com>
2023-05-07 00:38:11 +0530

    Permutations

--- src/arraydigitcount.java
+++ src/arraydigitcount.java
@@ -5,10 +5,11 @@ class numofdigits {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int []ar = new int[n];
+
         for (int i =0;i<n;i++){
             ar[i]= sc.nextInt();
-
         }
+
         int count=0;
         for (int i =0;i<n;i++){
             //int num = ar[i];
