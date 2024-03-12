58c6894d9aaf753b17275e96cf49f8b78dc3fc30
sarfarazalam13 <alamsarfaraz386@gmail.com>
2024-03-12 19:05:48 +0530

    prac

--- src/bubblesort.java
+++ src/bubblesort.java
@@ -1,16 +1,16 @@
-import java.lang.reflect.Array;
+/*import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class bubblesort {
     public static void main(String[] args) {
         int[] arr={1,2,3,4,5,6};
-                /*new int[5];
+                new int[5];
         Scanner in=new Scanner(System.in);
         for(int i=0;i<arr.length;i++)
         {
             arr[i]=in.nextInt();
-        }*/
+        }
         bubble(arr);
         System.out.println(Arrays.toString(arr));
 
@@ -39,5 +39,35 @@ public class bubblesort {
 
         }
 
+    }
+}*/
+
+import java.util.Arrays;
+
+class bubblesort{
+
+    static void bubble(int arr[])
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr.length-1-i;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    swap(arr,j,j+1);
+                }
+            }
+        }
+    }
+    static void swap(int arr[],int x,int y)
+    {
+        int temp=arr[x];
+        arr[x]=arr[y];
+        arr[y]=temp;
+    }
+    public static void main(String[] args) {
+        int[] arr={5,4,3,2,1};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
